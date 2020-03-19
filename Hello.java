080927d71ff3b327477d791607be94a5cf8fe3b2
Vinodh Kumar T <vinodh5052@gmail.com>
2020-03-19 21:36:26 +0530

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+// package com.vinodh;
+
+public class Hello { 
+
+public static void main(String[] args){
+	
+	System.out.println("Hello, Vinodh. Welcome to VIMS Editor !!");
+	
+}
+
+}
---
+++ module.js
@@ -0,0 +1,3 @@
+fun module(){
+	console.log("Hello, Module")
+}
